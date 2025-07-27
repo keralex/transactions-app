673c79787c77653ce07e609044dbd72483370914
keralex <kerlis.aguado26@gmail.com>
2025-07-26 23:56:44 -0300

    add revenue corner

--- src/components/organisms/Header/Header.styles.ts
+++ src/components/organisms/Header/Header.styles.ts
@@ -1,19 +1,19 @@
 export const headerContainerClass =
-  ' bg-white p-2 border-b border-[#DEE2EC] rounded-bl-[32px] h-corner z-200';
+  ' bg-white p-2 border-b border-[#DEE2EC] rounded-bl-[32px] h-corner-bottom z-200';
 
 export const cornerStyle = `
-    .h-corner::after {
-        content: '';
-        position: absolute;
-        top: 56px;
-        right: 0;
-        width: 32px;
-        height: 32px;
-        background-image: url('/app/assets/icons/h-corner.svg');
-        background-size: contain;
-        background-repeat: no-repeat;
-        pointer-events: none;
-    }
+  .h-corner-bottom::after {
+    content: '';
+    position: absolute;
+    top: 55px;
+    right: 0;
+    width: 32px;
+    height: 32px;
+    background-image: url('src/assets/icons/b-corner.svg'); 
+    background-size: contain;
+    background-repeat: no-repeat;
+    pointer-events: none;
+  }
 `;
 
 export const menuItems = [
--- src/components/organisms/RevenueSummary/RevenueSummary.tsx
+++ src/components/organisms/RevenueSummary/RevenueSummary.tsx
@@ -13,40 +13,42 @@ export const RevenueSummary: FC = () => {
     const { filter, setFilter } = useTransactionFilter();
     const { amountParts } = useFilteredTransactions();
     return (
-        <Container className="p-4 " isFlex direction="column" gap={4}>
-            <Text as='h1' variant='h1' className='text-text-title' weight='semibold'>Tus cobros</Text>
+        <div className='bg-white '>
+            <Container className=" bg-background-base p-4 border-r border-[#DEE2EC] rounded-tr-[32px] border-tl-transparent " isFlex direction="column" gap={4}>
+                <Text as='h1' variant='h1' className='text-text-title' weight='semibold'>Tus cobros</Text>
 
-            <div className="grid grid-cols-3 gap-0 text-sm font-medium w-full mt-6 mb-4">
-                {options.map((opt) => (
-                    <button
-                        key={opt.value}
-                        onClick={() => setFilter(opt.value)}
-                        className={`font-public px-5 py-3 text-text-light font-normal ${filter === opt.value && 'font-semibold relative'
-                            }`}
+                <div className="grid grid-cols-3 gap-0 text-sm font-medium w-full mt-6 mb-4">
+                    {options.map((opt) => (
+                        <button
+                            key={opt.value}
+                            onClick={() => setFilter(opt.value)}
+                            className={`font-public px-5 py-3 text-text-light font-normal ${filter === opt.value && 'font-semibold relative'
+                                }`}
+                        >
+                            {opt.label}
+                            {filter === opt.value && <div className="w-1 h-1 rounded-full bg-primary mx-auto mt-1" />}
+                        </button>
+                    ))}
+                </div>
+                <Container isFlex justify='center' align='center' className='mb-4'>
+                    <Text as='p' className="text-[34px] text-text-dark" >
+                        + $ {amountParts.integer}
+                        <Text as='span' className="text-[22px] ">,{amountParts.decimal}</Text>
+                    </Text>
+                </Container>
+                <Container isFlex justify='center' padding='lg'>
+                    <Link
+                        to="/metrics"
+                        iconName="analyze"
+                        iconSide="left"
+                        color="primary"
+                        size="md"
+                        isBold
                     >
-                        {opt.label}
-                        {filter === opt.value && <div className="w-1 h-1 rounded-full bg-primary mx-auto mt-1" />}
-                    </button>
-                ))}
-            </div>
-            <Container isFlex justify='center' align='center' className='mb-4'>
-                <Text as='p' className="text-[34px] text-text-dark" >
-                    + $ {amountParts.integer}
-                    <Text as='span' className="text-[22px] ">,{amountParts.decimal}</Text>
-                </Text>
-            </Container>
-            <Container isFlex justify='center' padding='lg'>
-                <Link
-                    to="/metrics"
-                    iconName="analyze"
-                    iconSide="left"
-                    color="primary"
-                    size="md"
-                    isBold
-                >
-                    Ver métricas
-                </Link>
-            </Container>
-        </Container >
+                        Ver métricas
+                    </Link>
+                </Container>
+            </Container >
+        </div>
     );
 };
